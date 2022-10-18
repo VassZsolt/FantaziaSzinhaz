1bfe4e372bda56c24859eeeb07ebf06d7aeb0184
Niki <mnikolett2001@gmail.com>
2022-10-18 10:14:46 +0200

    Modified the sql queries.

--- src/main/java/controller/MainPageController.java
+++ src/main/java/controller/MainPageController.java
@@ -43,7 +43,8 @@ public class MainPageController implements Initializable {
                     "inner join rendezo\n" +
                     "on szindarab.rendezoid=rendezo.rendezoid\n" +
                     "inner join helyszin\n" +
-                    "on eloadas.helyszin=helyszin.helyszinid";
+                    "on eloadas.helyszin=helyszin.helyszinid where eloadas.idopont>=DATE_ADD(CURRENT_DATE, INTERVAL -1 MONTH)\n"+
+                    "and eloadas.idopont<=DATE_ADD(CURRENT_DATE, INTERVAL +1 MONTH)";
             ResultSet rs = stmt.executeQuery(query);
 
             while (rs.next()) {
@@ -62,15 +63,12 @@ public class MainPageController implements Initializable {
         try {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/fantazia_szinhaz", "root", "");
             Statement stmt = conn.createStatement();
-            String query = "select eloadas.idopont as idopont, szindarab.nev as cim, mufaj.mufajnev as mufaj, rendezo.nev as szereplo, helyszin.nev as helyszin from eloadas inner join szindarab \n" +
-                    "on eloadas.szindarabid=szindarab.szindarabid\n" +
-                    "inner join mufaj\n" +
-                    "on szindarab.mufajid=mufaj.mufajid\n" +
-                    "inner join rendezo\n" +
-                    "on szindarab.rendezoid=rendezo.rendezoid\n" +
-                    "inner join helyszin\n" +
-                    "on eloadas.helyszin=helyszin.helyszinid\n" +
-                    "where eloadas.idopont<CURRENT_DATE()";
+            String query = "select eloadas.idopont as idopont, szindarab.nev as cim, mufaj.mufajnev as mufaj,\n"+
+                    "rendezo.nev as szereplo, helyszin.nev as helyszin from eloadas inner join szindarab\n"+
+                    "on eloadas.szindarabid=szindarab.szindarabid inner join mufaj on szindarab.mufajid=mufaj.mufajid\n"+
+                    "inner join rendezo on szindarab.rendezoid=rendezo.rendezoid inner join helyszin on\n"+
+                    "eloadas.helyszin=helyszin.helyszinid where eloadas.idopont>=DATE_ADD(CURRENT_DATE, INTERVAL -6 MONTH)\n"+
+                    "and eloadas.idopont<=CURRENT_DATE";
             ResultSet rs = stmt.executeQuery(query);
 
             while (rs.next()) {
@@ -107,15 +105,12 @@ public class MainPageController implements Initializable {
         try {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/fantazia_szinhaz", "root", "");
             Statement stmt = conn.createStatement();
-            String query = "select eloadas.idopont as idopont, szindarab.nev as cim, mufaj.mufajnev as mufaj, rendezo.nev as szereplo, helyszin.nev as helyszin from eloadas inner join szindarab \n" +
-                    "on eloadas.szindarabid=szindarab.szindarabid\n" +
-                    "inner join mufaj\n" +
-                    "on szindarab.mufajid=mufaj.mufajid\n" +
-                    "inner join rendezo\n" +
-                    "on szindarab.rendezoid=rendezo.rendezoid\n" +
-                    "inner join helyszin\n" +
-                    "on eloadas.helyszin=helyszin.helyszinid\n" +
-                    "where eloadas.idopont>CURRENT_DATE()";
+            String query = "select eloadas.idopont as idopont, szindarab.nev as cim, mufaj.mufajnev as mufaj,\n"+
+                    "rendezo.nev as szereplo, helyszin.nev as helyszin from eloadas inner join szindarab\n"+
+                    "on eloadas.szindarabid=szindarab.szindarabid inner join mufaj on szindarab.mufajid=mufaj.mufajid\n"+
+                    "inner join rendezo on szindarab.rendezoid=rendezo.rendezoid inner join helyszin on\n"+
+                    "eloadas.helyszin=helyszin.helyszinid where eloadas.idopont<=DATE_ADD(CURRENT_DATE, INTERVAL +12 MONTH)\n"+
+                    "and eloadas.idopont>=CURRENT_DATE";
             ResultSet rs = stmt.executeQuery(query);
 
             while (rs.next()) {
